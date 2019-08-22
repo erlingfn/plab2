9bcddcb21f28f20eff78a5e2b26356a7d4ae508a
erlingfn <erling.nesset@gmail.com>
2019-08-22 16:10:31 +0200

    add arduino code

---
+++ project1Arduino/project1Arduino.ino
@@ -0,0 +1,103 @@
+//pin vars
+int btnPin = 7; //pin to read btnState
+int dashPin = 8; //pin to light on dash
+int dotPin = 9; //pin to light on dot
+
+//vars to check if button state changes
+int btnVal = 0; 
+int lastBtnVal = 0;
+
+//timer vars
+unsigned long startMillis;
+unsigned long currentMillis;
+int timeDiff;
+
+//signal vars
+const unsigned long period = 500; //period agreed upon by sender and recievers
+int pause = 0;
+int signalToSend = 0;
+
+void setup() {
+    pinMode(btnPin, INPUT);
+    pinMode(dashPin, OUTPUT);
+    pinMode(dotPin, OUTPUT);
+    Serial.begin(9600);
+    //start timer
+    startMillis = millis();
+}
+
+//Return 0 for short signal, 1 for medium and 2 for long
+int calcPause(int mlsec) {
+    if(mlsec <= period) {
+        return 0;
+    } else if(mlsec <= 3 * period)  {
+        return 1;
+    } else if(mlsec <= 7 * period) {
+        return 2;
+    }
+}
+
+int calcSignal(int lastVal, int pause) {
+    //short pause
+    if(lastVal == 0 && pause == 0) {
+        return 0;
+    //medium pause
+    } else if(lastVal == 0 && pause == 1){
+        return 1;
+    //long pause
+    } else if(lastVal == 0 && pause == 2){
+        return 2;
+    //short push
+    } else if(lastVal == 1 && pause == 0){
+        digitalWrite(dotPin, HIGH);
+        return 3;
+    //medium push
+    } else if(lastVal == 1 && (pause == 1 || pause == 2 )){
+        digitalWrite(dashPin, HIGH);
+        return 4;
+    }
+}
+
+//check for btn debounce
+boolean debounceCheck(int mlsec) {
+    if(mlsec < 50) {
+        return true;
+    }
+    return false;
+}
+
+void loop() {
+    btnVal = digitalRead(btnPin);
+    if(btnVal == lastBtnVal){
+        currentMillis = millis();
+        timeDiff = currentMillis - startMillis;
+        if(timeDiff > 7000){
+            //reset LEDs
+            digitalWrite(dashPin, LOW);
+            digitalWrite(dotPin, LOW);
+        }
+        return;
+    } else {
+        currentMillis = millis();
+        //Serial.println(btnVal);
+        //Serial.println(startMillis);
+        timeDiff = currentMillis - startMillis;
+        if(debounceCheck(timeDiff)) {
+            return;
+        }
+        //reset LEDs
+        digitalWrite(dashPin, LOW);
+        digitalWrite(dotPin, LOW);
+        //find long short or medium pause
+        pause =  calcPause(timeDiff);
+        //calculate signal
+        signalToSend = calcSignal(lastBtnVal, pause);
+        //reset timer
+        startMillis = millis();
+        lastBtnVal = btnVal;
+        //do not send signal on short pause
+        if(signalToSend != 0){
+            Serial.println(signalToSend);
+        }
+    }
+}
