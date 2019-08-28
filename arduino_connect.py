5e01c4c776e0f357d0c1e66dfbaae39f456a6758
erlingfn <erling.nesset@gmail.com>
2019-08-28 20:25:09 +0200

    Add code for python part of project

--- arduino_connect.py
+++ arduino_connect.py
@@ -13,6 +13,8 @@ import serial
 
 # Connects to the arduino via a COM port (USB).
 # Does not work if the serial monitor in arduino is open.
+
+
 def pc_connect():
     for i in range(100):
         try:
@@ -22,12 +24,14 @@ def pc_connect():
         except serial.SerialException:
             pass
     exit("Arduino was not found")
-    
-# **** For MACs ******    
 
-# arport = Arduino device port, which you can find at the bottom of your arduino window or via Arduino menu options tools/port.
-#   The default will probably NOT work for your machine, but it may look quite similar, differing only
-#  in the final 4 digits.
+# **** For MACs ******
 
-def basic_connect(arport='/dev/cu.usbmodem1451'):
-    return serial.Serial(arport,9600,timeout=.1)
+# arport = Arduino device port, which you can find at the bottom of your arduino window
+# or via Arduino menu options tools/port.
+#  The default will probably NOT work for your machine, but it may look quite similar,
+# differing only in the final 4 digits.
+
+
+def basic_connect(arport='/dev/ttyUSB0'):
+    return serial.Serial(arport, 9600, timeout=.1)
